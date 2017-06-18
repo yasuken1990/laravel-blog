5a10b7ced00e4f31cfe9a9c2f44b33f096cb0515
yasuken <yasuken@moremost.jp>
2017-06-19 06:38:14 +0900

    [modify]Category, Post fillable追加

--- app/Category.php
+++ app/Category.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    /**
-     * TODO: fix
-     * 何がNONEなのかわからない。
-     * そもそもこれは何のために必要な定数なのか。ナンだけに。
-     */
-    // カテゴリが何も先手腐れていない場合は、categories.id = 1 のカテゴリを使用します。
+    const PAGINATION = 10;
+
+    protected $fillable = ['name'];
+
 }
