3dce64a1568510c6a97eb44707451271ceaaa3d8
yasuken <yasuken@moremost.jp>
2017-07-03 07:31:45 +0900

    [fix]フロントエラー処理の追加

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,14 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Site;
 use App\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Log;
 
 class FrontController extends Controller
 {
     //
     public function index()
     {
-        $site = Site::find(1);
-        $posts = Post::all();
-        return view('index', compact('site', 'posts'));
+        try {
+            $site = Site::firstOrFail();
+            $posts = Post::all();
+
+            return view('index', compact('site', 'posts'));
+
+        } catch (ModelNotFoundException $e) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+
+            return abort(404);
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            Log::error($e->getTraceAsString());
+
+            // TODO フロントだし、500がいいかな…
+            // https://readouble.com/laravel/5.4/ja/errors.html
+            return abort(404);
+        }
     }
 }
